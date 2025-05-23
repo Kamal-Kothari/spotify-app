2c07f33af85478de19be22a7d87e687af7159371
Kamal <kamallogin8@gmail.com>
2025-05-23 11:40:26 +0530

    shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
-  
-
   return (
-    <>
-      <h1 className="text-blue-800 text-center text-5xl">Home</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh" >
+      <Button variant={"link"}>Click me</Button>
+    </div>
   )
 }
 
